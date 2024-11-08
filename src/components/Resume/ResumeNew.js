4cb33429cb03c38df2bc93d669ab8becc471daf9
Lovesh <83826365+Lovesh1@users.noreply.github.com>
2024-11-08 18:57:39 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Lovesh's Resume-hackerresume (3).pdf";
+import pdf from "../../Assets/LoveshSharmaResume (3).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
